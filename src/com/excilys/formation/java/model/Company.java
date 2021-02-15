ecd722159445c19d77ffd02d627565a74cf5be83
felixg03 <fgyr@excilys.com>
2021-02-15 17:13:59 +0100

    creation of classes Main, Company and Computer

---
+++ src/com/excilys/formation/java/model/Company.java
@@ -0,0 +1,30 @@
+package com.excilys.formation.java.model;
+
+import java.security.Timestamp;
+
+public class Company {
+	
+	private long id;
+	private String name;
+	
+	// GETTERS AND SETTERS
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public Company(long id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+}
---
+++ src/com/excilys/formation/java/model/Computer.java
@@ -0,0 +1,57 @@
+package com.excilys.formation.java.model;
+
+import java.security.Timestamp;
+
+public class Computer {
+	private long id;
+	private String name;
+	private Timestamp introduced;
+	private Timestamp discontinued;
+	private long company_id;
+	
+	// GETTERS AND SETTERS
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Timestamp getIntroduced() {
+		return introduced;
+	}
+	public void setIntroduced(Timestamp introduced) {
+		this.introduced = introduced;
+	}
+	public Timestamp getDiscontinued() {
+		return discontinued;
+	}
+	public void setDiscontinued(Timestamp discontinued) {
+		this.discontinued = discontinued;
+	}
+	public long getCompany_id() {
+		return company_id;
+	}
+	public void setCompany_id(long company_id) {
+		this.company_id = company_id;
+	}
+	
+	
+	
+	public Computer(long id, String name, Timestamp introduced, 
+					Timestamp discontinued, long company_id) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.introduced = introduced;
+		this.discontinued = discontinued;
+		this.company_id = company_id;
+	}
+	
+	
+}
